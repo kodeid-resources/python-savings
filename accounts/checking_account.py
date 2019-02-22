5a33a4b5e1b1ad7330b594c11b230761272a78c5
Riza Fahmi <rizafahmi@gmail.com>
2019-02-22 16:42:13 +0700

    Lesson 5: Wired up all together

--- accounts/checking_account.py
+++ accounts/checking_account.py
@@ -2,5 +2,4 @@ from .bank_account import BankAccount
 
 
 class CheckingAccount(BankAccount):
-    def __init__(self, balance):
-        super().__init__(self, balance)
+    pass
--- runner.py
+++ runner.py
@@ -1,3 +1,7 @@
+from accounts.savings_account import SavingsAccount
+from accounts.checking_account import CheckingAccount
+
+
 def main():
     # Setup accounts
     savings_account = SavingsAccount(100000)
@@ -5,11 +9,13 @@ def main():
 
     # Check balance
     print('Savings account balance: Rp. ', savings_account.get_balance())
-    print('Checking account balance: Rp. {}\n'.format(checking_account.get_balance))
+    print('Checking account balance: Rp. {}\n'.format(checking_account.get_balance()))
 
     # Transfer
     savings_account.transfer(checking_account, 100000)
 
     # Check balance
     print('Savings account balance: Rp. ', savings_account.get_balance())
-    print('Checking account balance: Rp. {}\n'.format(checking_account.get_balance))
+    print('Checking account balance: Rp. {}\n'.format(checking_account.get_balance()))
+
+main()
